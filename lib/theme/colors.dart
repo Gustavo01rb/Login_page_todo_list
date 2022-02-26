0ebff6ae23d25e24e6c3588ee9435ea725132718
Gustavo Barcelos <gustavo05rb@gmail.com>
2022-02-25 23:23:52 -0300

    Commit inicial

---
+++ lib/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors{
+  Color primary       = const Color(0xff4B50BF);
+  Color darkPrimary   = const Color(0xff231773);
+  Color lightPrimary  = const Color(0xff6E30F2);
+
+  Color secondary     = const Color(0xffEBEDF2);
+  Color darkSecondary = const Color(0xffACB0F2);
+}
